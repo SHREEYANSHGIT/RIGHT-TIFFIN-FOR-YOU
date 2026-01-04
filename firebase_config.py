a15b43068cb3cba0782193c7db79597dcdc53daf
SHREEYANSHGIT <shreeyanshgithub18@gmail.com>
2026-01-05 00:36:54 +0530

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -3,8 +3,26 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 if not firebase_admin._apps:
-    # Use to_dict() method
-    cred = credentials.Certificate(st.secrets["firebase"].to_dict())
+    # Get the private key and fix newlines
+    private_key = st.secrets["firebase"]["private_key"]
+    
+    # THIS IS THE FIX - convert \n to actual newlines
+    private_key = private_key.replace("\\n", "\n")
+    
+    key_dict = {
+        "type": st.secrets["firebase"]["type"],
+        "project_id": st.secrets["firebase"]["project_id"],
+        "private_key_id": st.secrets["firebase"]["private_key_id"],
+        "private_key": private_key,  # Use fixed key
+        "client_email": st.secrets["firebase"]["client_email"],
+        "client_id": st.secrets["firebase"]["client_id"],
+        "auth_uri": st.secrets["firebase"]["auth_uri"],
+        "token_uri": st.secrets["firebase"]["token_uri"],
+        "auth_provider_x509_cert_url": st.secrets["firebase"]["auth_provider_x509_cert_url"],
+        "client_x509_cert_url": st.secrets["firebase"]["client_x509_cert_url"]
+    }
+    
+    cred = credentials.Certificate(key_dict)
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
