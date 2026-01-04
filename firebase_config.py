6c5c04999f2a6d60ff028d9d48aa608a52a672d0
SHREEYANSHGIT <shreeyanshgithub18@gmail.com>
2026-01-05 00:17:18 +0530

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -2,9 +2,9 @@ import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-# Initialize Firebase only once
 if not firebase_admin._apps:
-    cred = credentials.Certificate(st.secrets["firebase"])
+    # Use to_dict() method
+    cred = credentials.Certificate(st.secrets["firebase"].to_dict())
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
